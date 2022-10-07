1bef42e38d96125608d0ba0bf01e5f5b2e42ac5e
Muhammed Esad Goncaloğlu <93635211+MEG46@users.noreply.github.com>
2022-10-07 21:28:17 +0300

    Update Calculator_In_C.c
    You can do four basic mathematical process with this calculator. I hope you like it.
    
    Signed-off-by: Muhammed Esad Goncaloğlu <93635211+MEG46@users.noreply.github.com>

--- Calculator_In_C.c
+++ Calculator_In_C.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main(){
 	char c;
+	char ch;
+	do{
 	int num1,num2;
 	printf("Please enter a number : ");
 	scanf("%d",&num1);
@@ -18,11 +20,18 @@ int main(){
 	}else if(c=='*'){
 		printf("%d * %d = %d\n",num1,num2,(num1*num2));
 	}else if(c=='/'){
+		if(num==0){
 		printf("%d / %d = %d\n",num1,num2,(num1/num2));
+		}
 	}else if(c=='%'){
 		printf("%d %% %d = %d\n",num1,num2,(num1%num2));
 	}else{
 		printf("Invalid Process!");
 	}
+	fflush(stdin);
+	printf("Do you want to continue ? (Y / N) : ");
+	scanf("%c",&ch);
+	fflush(stdin);
+	}while(ch!='N');
 	return 0;
 }
