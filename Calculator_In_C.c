2a1ccbee1216176482e6fb20fed26cd1bd1b7eac
Muhammed Esad Goncaloğlu <93635211+MEG46@users.noreply.github.com>
2022-10-07 21:57:49 +0300

    Update Calculator_In_C.c
    Signed-off-by: Muhammed Esad Goncaloğlu <93635211+MEG46@users.noreply.github.com>

--- Calculator_In_C.c
+++ Calculator_In_C.c
@@ -14,21 +14,23 @@ int main(){
 	scanf("%lf",&num2);
 	fflush(stdin);
 	if(c=='+'){
-		printf("%lf + %lf = %.3lf\n",num1,num2,(num1+num2));
+		printf("%lf + %lf = %.5lf\n",num1,num2,(num1+num2));
 	}else if(c=='-'){
-		printf("%%lf - %lf = %.3lf\n\n",num1,num2,(num1-num2));
+		printf("%lf - %lf = %.5lf\n\n",num1,num2,(num1-num2));
+	}else if(c=='x'){
+		printf("%lf x %lf = %.5lf\n\n",num1,num2,(num1*num2));
 	}else if(c=='*'){
-		printf("%lf x %lf = %.3lf\n\n",num1,num2,(num1*num2));
+		printf("%lf x %lf = %.5lf\n\n",num1,num2,(num1*num2));
 	}else if(c=='/'){
-		if(num==0){
+		if(num2==0){
 			printf("Couldn't be divided by 0.\n");
 		}
-		printf("%lf / %lf = %.3lf\n\n",num1,num2,(num1/num2));
-		}
+		printf("%lf / %lf = %.5lf\n\n",num1,num2,(num1/num2));
+		
 	}else if(c=='%'){
 		printf("%d %% %d = %d\n",(int)num1,(int)num2,((int)num1%(int)num2));
 	}else{
-		printf("Invalid Process!");
+		printf("Invalid Process!\n");
 	}
 	fflush(stdin);
 	printf("Do you want to continue ? (Y / N) : ");
